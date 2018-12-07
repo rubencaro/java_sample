793adce061b0e638592c73521bff4889b2bc74b3
Rubén Caro <ruben.caro.estevez@gmail.com>
2018-12-07 15:52:29 +0100

    All passing ok

--- src/test/java/java_sample/AppTest.java
+++ src/test/java/java_sample/AppTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 public class AppTest {
     @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        App app = new App();
+        assertNotNull("app should have a greeting", app.getGreeting());
     }
 }
